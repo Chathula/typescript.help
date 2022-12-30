bb82122d9938de84321ec676a27eea741d9f767e
Chathula <schathula@gmail.com>
2022-12-30 12:04:56 +0100

    updated postcss build

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      '@fullhuman/postcss-purgecss': {
+        content: [
+          '../node_modules/pliny/**/*.{js,ts,tsx}',
+          './node_modules/pliny/**/*.{js,ts,tsx}',
+          './pages/**/*.{js,ts,tsx}',
+          './components/**/*.{js,ts,tsx}',
+          './layouts/**/*.{js,ts,tsx}',
+          './lib/**/*.{js,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+      'postcss-preset-env': {},
+    },
   },
 }
